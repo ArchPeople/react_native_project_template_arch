164e2e2ae8b6aecb8ff56cec95aa95043ccb1548
michmadheo <michaelamadheo@gmail.com>
2025-09-12 21:11:06 +0700

    ver:1.0.3 msg:Rename back to scaffold/Scaffold.tsx

--- src/app/components/organisms/index.ts
+++ src/app/components/organisms/index.ts
@@ -1,3 +1,3 @@
 export { AppBar } from './app-bar/AppBar';
-export { Scaffold } from './scaffold/TempScaffold';
+export { Scaffold } from './scaffold/Scaffold';
 export { StatusController } from './status-controller/StatusController';
