fea20d766eb0f3206cc6bb1a628f13d27e6eea96
michmadheo <michaelamadheo@gmail.com>
2025-09-12 21:09:11 +0700

    ver:1.0.3 msg:Rename Scaffold for temporary

--- src/app/components/organisms/index.ts
+++ src/app/components/organisms/index.ts
@@ -1,3 +1,3 @@
 export { AppBar } from './app-bar/AppBar';
-export { Scaffold } from './scaffold/Scaffold';
+export { Scaffold } from './scaffold/TempScaffold';
 export { StatusController } from './status-controller/StatusController';
