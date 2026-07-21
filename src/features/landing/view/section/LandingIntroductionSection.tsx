7993df876b15cbdc55300bf713087bb9b3ca7c8b
michmadheo <michaelamadheo@gmail.com>
2026-07-21 23:10:39 +0700

    ver:2.1.1 msg:Update template version number to 2.1.1

--- src/features/landing/view/section/LandingIntroductionSection.tsx
+++ src/features/landing/view/section/LandingIntroductionSection.tsx
@@ -133,7 +133,7 @@ export const LandingIntroductionSection: React.FC<LandingIntroductionSectionProp
         </View>
         <Gap height={ds(20)} />
         <TextBase style={[themeFonts.bodyXsLight, { textAlign: 'center' }]}>
-          ver 2.1.0
+          ver 2.1.1
         </TextBase>
         <Gap height={ds(screenSizeUtil.getBottomBarHeight) + ds(20)} />
       </GradientView>
